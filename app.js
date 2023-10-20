5fe476a30fe969bccc4e7c80e0d87ddf0cf5903e
GalisGalisteo <galis.travelling@gmail.com>
2023-10-20 14:16:00 +0200

    ex finished with free style :)

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// ¿En tu páis de origen es normal dejar propina?
+
+const calculateButtonNode = document.querySelector('#calculate');
+const resetButtonNode = document.querySelector('#reset');
+
+calculateButtonNode.addEventListener('click', () => {
+    const billAmountInput = parseInt(document.querySelector('#bill-amount').value);
+    const percentageTipInput = parseInt(document.querySelector('#percentage-tip').value);
+
+    const tipAmountOutput = document.querySelector('#tip-amount');
+    const totalOutput = document.querySelector('#total');
+
+    const tipAmount = billAmountInput * percentageTipInput / 100;
+    const total = billAmountInput + tipAmount;
+
+    tipAmountOutput.value = tipAmount;
+    totalOutput.value = total;
+})
+
+resetButtonNode.addEventListener('click', () => {
+    location.reload();
+})
